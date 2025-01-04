dcc7a54daecc4457325371adfca835064442ad03
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-01-04 21:40:26 +0200

    added footer, enhanced the ui (v1)

--- views/layout.js
+++ views/layout.js
@@ -1,6 +1,9 @@
 let menuIcon = document.querySelector('.fa-bars')
+let navigationBar = document.querySelector('.bar')
+let listItem = document.getElementsByClassName('link')
 
 menuIcon.addEventListener("click", ()=>{
        menuIcon.classList.toggle('fa-times')
+       navigationBar.classList.toggle('active')
 })
 

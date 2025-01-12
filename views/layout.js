b3d94ad69415b612d1baa6cfceba0b2a3dfb80bb
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-01-12 18:54:12 +0200

    changed color, improved the responsive navbar, centered content on mobile, the link appears after the user clicked on the send button.

--- views/layout.js
+++ views/layout.js
@@ -1,10 +1,35 @@
 let menuIcon = document.querySelector('.fa-bars')
 let navigationBar = document.querySelector('.bar')
-let listItem = document.getElementsByClassName('link')
+
+let listItem = document.querySelectorAll('.link')
+
+let showData = document.querySelector('.data-response')
+let send = document.querySelector('.fa-paper-plane')
+
 
 menuIcon.addEventListener("click", ()=>{
        menuIcon.classList.toggle('fa-times')
        navigationBar.classList.toggle('active')
 })
 
-document.querySelector('.data-response').classList.ad('hidden')
+showData.classList.add('hidden')
+
+send.addEventListener('click', (event)=>{
+       event.preventDefault()
+
+setTimeout(() => {
+       showData.classList.remove('hidden')
+}, 2000); // 2000 milliseconds = 2 seconds
+
+setTimeout(() => {
+       showData.classList.add('hidden')
+}, 30000); 
+})  
+
+
+listItem.forEach((item)=>{
+       item.addEventListener("click", (event)=>{
+              menuIcon.classList.remove('fa-times')
+              navigationBar.classList.remove('active')
+       })
+})

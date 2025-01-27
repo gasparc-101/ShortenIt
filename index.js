93c7f19ff3414e3e4fb570c2ee5aa95884073804
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-01-27 13:56:38 +0200

    hidden sensitive data

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const server_email = process.env.EMAIL_ADDRESS;
 const server_email_pass = process.env.EMAIL_PASSWORD;
 
 let shortened = ''
+let link = ''
 
 const config = {
     headers: { Authorization: `Bearer ${BearerToken}`,
@@ -30,7 +31,7 @@ app.use(express.json());
 
 // Route handlers
 app.get("/", (req, res) => {
-    res.render("index.ejs", {shortened : shortened});
+    res.render("index.ejs", {shortened : shortened,  error : "An error occured while trying to shorten the link", link : link});
 });
 
 app.get("/about", (req, res) => {
@@ -45,12 +46,13 @@ app.get("/feedback", (req, res) => {
 
 app.post("/submit-link", async (req, res)=>{
 
+    let link = req.body.link
     console.log(req.body);
     try {
-        const url = req.body.link;  // Extract the long URL from the request body
+        const url = req.body.link;
         console.log(url);
     
-        // Use POST request to the TinyURL API
+        // POST request to the TinyURL API
         const response = await axios.post(
             LINK_URL,
             {
@@ -65,62 +67,66 @@ app.post("/submit-link", async (req, res)=>{
         
         console.log(response.data.data.tiny_url);
     
-        // You can send the shortened URL or other relevant data to the client
+        // send the shortened URL or other relevant data to the client
 
         res.redirect('/')
         
 
         } catch (error) {
-            console.log(`Error: ${error.message}`);
-            console.log(error.response?.data);
 
-            res.status(500).send("An error occurred while shortening the link.");
+        
+            console.log(`Error: ${error.message}`);
+            //console.log(error.response?.data);
+            res.send("an error occured while trying to shorten the link")
         }
 });
 
 app.post('/send-email',(req, res)=>{
     console.log(req.body)
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: server_email,
-            pass: server_email_pass
-        }
-    })
 
-    async function main() {
-        const info = await transporter.sendMail({
-            from : `"Gaspar, from Syntaxis" <${server_email}>`,
-            to: req.body.email,
-            subject : `Your feedback has been received!`,
-            html: `
-            <h2>Thank you for your feedback!</h2>
-            <p>Hi, ${req.body.full_name}!</p>
-            <p>We’ve received your feedback with the subject <b>${req.body.subject}</b> and it will be taken into consideration for future updates to the app.</p>
-            <p>Your input is invaluable to us, and we’re always working to improve the user experience.</p>
-            <p>Best regards, <br> The Syntaxis Team</p>
+    try {
 
-            `,
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: server_email,
+                pass: server_email_pass
+            }
         })
 
+        async function main() {
+            const info = await transporter.sendMail({
+                from : `"Gaspar, from Syntaxis" <${server_email}>`,
+                to: req.body.email,
+                subject : `Your feedback has been received!`,
+                html: `
+                <h2>Thank you for your feedback!</h2>
+                <p>Hi, ${req.body.full_name}!</p>
+                <p>We’ve received your feedback with the subject <b>${req.body.subject}</b> and it will be taken into consideration for future updates to the app.</p>
+                <p>Your input is invaluable to us, and we’re always working to improve the user experience.</p>
+                <p>Best regards, <br> The Syntaxis Team</p>
 
-        // sending data to google forms
+                `,
+            })
 
-        const formData = new URLSearchParams();
-        formData.append('entry.564836964', req.body.full_name)
-        formData.append('entry.123566139', req.body.email)
-        formData.append('entry.64796350', req.body.subject)
-        formData.append('entry.184176017', req.body.message)
 
-        const googleFormURL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfHoEghmSgBW8E14VRUqLO6hibuGK0aYiP5_V48wmMplmeKbA/formResponse';
+            // sending data to google forms
 
-        const response = await fetch(googleFormURL, {
-            method: 'POST',
-            body: formData,
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            const formData = new URLSearchParams();
+            formData.append('entry.564836964', req.body.full_name)
+            formData.append('entry.123566139', req.body.email)
+            formData.append('entry.64796350', req.body.subject)
+            formData.append('entry.184176017', req.body.message)
+
+            const googleFormURL = process.env.GOOGLE_FORMS_URL;
+
+            const response = await fetch(googleFormURL, {
+                method: 'POST',
+                body: formData,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         })
 
 
@@ -129,7 +135,14 @@ app.post('/send-email',(req, res)=>{
         console.log('Form submitted to Google Form (preview):', result.substring(0, 200)); // Exibe os primeiros 200 caracteres
     }
 
-    main().catch(console.error);
+        res.render('feedback')
+
+        main().catch(console.error);
+        
+    } catch (error) {
+        console.log(error.message)
+        res.render('feedback')
+    }
 
 })
 

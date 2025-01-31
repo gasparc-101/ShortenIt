275ba88d4b1ee80278f13e3bad24bd78d5128aae
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-01-31 18:01:14 +0200

    fixed file structure

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ dotenv.config();
 
 import nodemailer from "nodemailer"
 import fetch from "node-fetch"
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -33,6 +40,7 @@ const config = {
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.json());
